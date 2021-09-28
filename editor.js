4d1d301f1631fcca8001477d8c064aea7984f938
YmmSgy <briansujl@gmail.com>
2021-09-28 11:07:44 +0800

    Completed intermediate styling
    Added css variables for theme colours.
    Changed background colours, borders, word sizes, margins and paddings, hover and disabled colours, drop shadows, and transitions to fit design philosophy.
    Used flexbox to control element sizing and scaling.

--- editor.js
+++ editor.js
@@ -1,10 +1,51 @@
-let fname = document.getElementById("fname");
-let fcont = document.getElementById("fcont");
-let save = document.getElementById("save");
+// file name and content
+const fname = document.getElementById("fname");
+const fcont = document.getElementById("fcont");
+// menu button
+const menu = document.getElementById("menu");
+// sidebar
+const sidebar = document.getElementById("sidebar");
+const save = document.getElementById("save");
+/*
+const open = document.getElementById("open");
+const dark = document getElementById("dark");
+const font = document.getElementById("font");
+const hl = document.getElementById("hl");
+const indent = document.getElementById("indent");
+*/
+// globals
+let isSidebarOpen = false;
 
-save.onclick = function() {
+function getvar(name) {
+    return getComputedStyle(
+      document.documentElement)
+        .getPropertyValue(name);
+}
+
+function setvar(name, value) {
+    document.documentElement.style
+      .setProperty(name, value);
+}
+
+function openSidebar() {
+    fname.setAttribute("disabled", "");
+    fcont.setAttribute("disabled", "");
+    menu.style.backgroundColor = getvar("--sbr-clr");
+    sidebar.style.height = "100%";
+    isSidebarOpen = true;
+}
+
+function closeSidebar() {
+    fname.removeAttribute("disabled");
+    fcont.removeAttribute("disabled");
+    menu.style.backgroundColor = "transparent";
+    sidebar.style.height = "0";
+    isSidebarOpen = false;
+}
+
+function download() {
     // create fake anchor
-    let dl = document.createElement("a");
+    const dl = document.createElement("a");
     // dynamically set attributes of
     // fake anchor
     dl.setAttribute("href",
@@ -18,4 +59,10 @@ save.onclick = function() {
     dl.click();
     // remove fake anchor
     document.body.removeChild(dl);
+}
+
+menu.onclick = () => {
+    if (isSidebarOpen) { closeSidebar(); }
+    else { openSidebar(); }
 };
+save.onclick = download;
