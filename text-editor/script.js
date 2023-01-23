ae9c200efdb0c297080b443b995bd8b5ed7a04e5
YmmSgy <briansujl@gmail.com>
2023-01-23 09:35:38 +0800

    text-editor: cleaned up sidebar

--- text-editor/script.js
+++ text-editor/script.js
@@ -2,32 +2,34 @@
 
 const menubutton = document.getElementById("menubutton");
 const menu = document.getElementById("menu");
+const curtain = document.getElementById("curtain");
+const menuClose = document.getElementById("menu-close");
 
 let isMenuOpen = false;
 
 function openMenu() {
 	isMenuOpen = true;
 
-	menu.style.display = "block";
-
-	menubutton.style.position = "relative";
-	menubutton.style.backgroundColor = "gainsboro";
-	menubutton.style.boxShadow = "0 0 0.5rem grey";
+	curtain.style.zIndex = 1;
+	curtain.style.backgroundColor = "rgb(0 0 0 / .6)";
+	menu.style.left = "0";
+	curtain.classList.toggle('curtain-fade-in');
+	curtain.classList.toggle('curtain-fade-out');
 }
 
 function closeMenu() {
 	isMenuOpen = false;
 
-	menu.style.display = "none";
-
-	menubutton.style = null;
+	curtain.style.zIndex = -1;
+	curtain.style.backgroundColor = "transparent";
+	menu.style.left = "-40%";
+	curtain.classList.toggle('curtain-fade-in');
+	curtain.classList.toggle('curtain-fade-out');
 }
 
-menubutton.addEventListener("click", () => {
-	if (isMenuOpen) { closeMenu(); }
-	else { openMenu(); }
-});
-menubutton.addEventListener("blur", closeMenu);
+menubutton.addEventListener("click", openMenu);
+menuClose.addEventListener("click", closeMenu);
+curtain.addEventListener("click", closeMenu);
 
 function setvar(property, value) {
 	document.documentElement.style.setProperty(property, value);
